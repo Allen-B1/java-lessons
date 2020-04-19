f29968fe180a4647c9b07c423a1d590189599240
Allen B <pingguo.ny@gmail.com>
2020-04-18 23:01:15 -0400

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/allen-b1/java-lessons
+
+go 1.13
---
+++ script.js
@@ -0,0 +1,17 @@
+const hljs = require('highlightjs');
+const md = require('markdown-it')({
+	breaks: true,
+	linkify: true,
+}).use(require('markdown-it-mermaid').default);
+
+const fs = require('fs');
+const TEMPLATE = fs.readFileSync("template.html", {encoding:"utf8"});
+var files = fs.readdirSync(".");
+for (let file of files) {
+	if (file.endsWith(".md")) {
+		let content = fs.readFileSync(file, {encoding:"utf8"});
+		let rendered = md.render(content);
+		rendered = TEMPLATE.replace(/\[TITLE\]/g, file.slice(0,-3)).replace(/\[CONTENT\]/g, rendered);
+		fs.writeFileSync("out/" + file.slice(0,-3) + ".html", rendered);
+	}
+}
